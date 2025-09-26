40010a83e5a644ade71d09569d3cb66f0ae2f90c
victor <6280636@cegepmontpetit.ca>
2025-09-26 14:32:26 -0400

    Cambio en mostrar_mensaje desde rama A

--- testapp/views.py
+++ testapp/views.py
@@ -3,7 +3,6 @@ from django.http import HttpResponse
 from .models import Mensaje
 from datetime import datetime
 
-def mostrar_mensaje(request):
-    ahora = datetime.now().strftime("%H:%M:%S")
-    return HttpResponse(f"<h1>Hora actual: {ahora}</h1><p>Esta es la rama de prueba hora.</p>")
+def mostrar_mensaje(request):   
+    return HttpResponse("<h1>Mensaje desde rama A</h1>")
 # Create your views here.
