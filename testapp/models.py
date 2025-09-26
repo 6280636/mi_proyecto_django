a77df823697278dc8ab1d010e2de907f1dc0f553
victor <6280636@cegepmontpetit.ca>
2025-09-26 12:54:06 -0400

    Agregado modelo Mensaje

--- testapp/models.py
+++ testapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Mensaje(models.Model):
+    texto = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.texto
