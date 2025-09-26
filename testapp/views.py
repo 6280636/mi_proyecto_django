44e5d78d44801db0e26e3f49c1a8eb52444208b6
victor <6280636@cegepmontpetit.ca>
2025-09-26 12:59:50 -0400

    Agregada función mostrar_mensaje en views.py

--- testapp/views.py
+++ testapp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Mensaje
+
+def mostrar_mensaje(request):
+    m = Mensaje(texto="Hola, este es un mensaje de prueba")
+    return HttpResponse(m.texto)
 
 # Create your views here.
